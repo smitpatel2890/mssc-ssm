78337cee4a63fb0d73722ae6845ba55e22bdf2d0
Smit Patel <smit.patel@hitachivantara.com>
2023-03-18 21:29:19 +0530

    impl notification actions

--- src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -3,6 +3,7 @@ package guru.springframework.msscssm.config;
 import guru.springframework.msscssm.domain.PaymentEvent;
 import guru.springframework.msscssm.domain.PaymentState;
 import guru.springframework.msscssm.services.PaymentServiceImpl;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.support.MessageBuilder;
@@ -20,9 +21,18 @@ import java.util.EnumSet;
 import java.util.Random;
 
 @Slf4j
+@RequiredArgsConstructor
 @EnableStateMachineFactory
 @Configuration
 public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+
+    private final Action<PaymentState, PaymentEvent> preAuthAction;
+    private final Action<PaymentState, PaymentEvent> authAction;
+    private final Guard<PaymentState, PaymentEvent> paymentIdGuard;
+    private final Action<PaymentState, PaymentEvent> preAuthApprovedAction;
+    private final Action<PaymentState, PaymentEvent> preAuthDeclinedAction;
+    private final Action<PaymentState, PaymentEvent> authApprovedAction;
+    private final Action<PaymentState, PaymentEvent> authDeclinedAction;
 
     @Override
     public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
@@ -37,19 +47,23 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
     @Override
     public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
         transitions.withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
-                .action(preAuthAction()).guard(paymentIdGuard())
+                .action(preAuthAction).guard(paymentIdGuard)
                 .and()
                 .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
+                .action(preAuthApprovedAction)
                 .and()
                 .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.PRE_AUTH_DECLINED)
+                .action(preAuthDeclinedAction)
                 //preauth to auth
                 .and()
                 .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.PRE_AUTH).event(PaymentEvent.AUTHORIZE)
-                .action(authAction())
+                .action(authAction)
                 .and()
                 .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH).event(PaymentEvent.AUTH_APPROVED)
+                .action(authApprovedAction)
                 .and()
-                .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH_ERROR).event(PaymentEvent.AUTH_DECLINED);
+                .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH_ERROR).event(PaymentEvent.AUTH_DECLINED)
+                .action(authDeclinedAction);
     }
 
     @Override
@@ -71,7 +85,7 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
         };
     }
 
-    public Action<PaymentState, PaymentEvent> preAuthAction(){
+    /*public Action<PaymentState, PaymentEvent> preAuthAction(){
         return context -> {
             System.out.println("PreAuth was called!!!");
 
@@ -88,9 +102,9 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
                         .build());
             }
         };
-    }
+    }*/
 
-    public Action<PaymentState, PaymentEvent> authAction(){
+    /*public Action<PaymentState, PaymentEvent> authAction(){
         return context -> {
             System.out.println("Auth was called!!!");
 
@@ -107,5 +121,5 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
                         .build());
             }
         };
-    }
+    }*/
 }
